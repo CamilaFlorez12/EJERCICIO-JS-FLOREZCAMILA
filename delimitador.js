ef219fb1738bc8b5ac5551aa655c9247e0e3e2e5
CamilaFlorez12 <07camilaflorezsantos@gmail.com>
2025-06-01 16:51:33 -0500

    fix: :bug: fixed exercise
    fixed function because before the program was not functional
    
    By @CamilaFlorez

--- delimitador.js
+++ delimitador.js
@@ -1,38 +1,44 @@
-
-const delimitador=prompt("Ingrese una expresion para ver si esta equilibrada:");
-function equilibrada(){
-    const pilares={
-        "{": "}",
-        "(":")",
-        "[":"]"
-    }
-    for (let i = 0; i < pilares.length; i++) {
-        let caracter = delimitador[i];
-        if (pilares[caracter]!== pilares[caracter]){
-            alert("La expresion no esta equilibrada")
-            return false;
-        }else if (Object.values(pilares).includes(caracter)){
-            if (pila.length ===0){
-                return false;
-            }else if (Object.keys(pilares).includes(caracter)){
-                let ultimo = delimitadorindexOf(caracter);
-                if (ultimo === -1){
-                    alert("La expresion no esta equilibrada")
-                    return false;
-                }
-                else if (pilares[ultimo]!== caracter){
-                    alert("La expresion no esta equilibrada")
-                    return false;
-                }else if(pilares[1][-1]!== delimitador.has(caracter)){
-                    alert("La expresion no esta equilibrada")
-                    return false;
-                    
-                }else{
-                    return true;
-                }
+const delimitador={
+    "{":"}",
+    "[":"]",
+    "(":")"
+};
+function menu(){
+    return Number(prompt("Ingrese una opcion:\n1. Saber si la expresion esta equilibrada\n2. Salir"))
+}
+function identificaDelimitadores(){
+    const pedir=prompt("Ingrese una expresion para saber si esta equilibrada:");
+    const pares=[];
+    for(let i=0; i< pedir.length;i++){
+        const delimitante=pedir[i];
+        if (delimitante ==="{"||delimitante==="["||delimitante==="("){
+            pares.push(delimitante);
+        }else if(delimitante ==="}"||delimitante==="]"||delimitante===")"){
+            const ultimo= pares.pop();
+            if(delimitador[ultimo] !== delimitante || ultimo===undefined){
+                alert("La expresion no esta equilibrada");
+                return;
             }
         }
-
+        }
+    if (pares.length===0){
+        alert("La expresion esta equilibrada");
+    } else{
+        alert("La expresion no esta equilibrada");
     }
-
+};
+let opcion=menu();
+while(opcion!==2){
+    switch(opcion){
+        case 1:
+            identificaDelimitadores();
+            break;
+        case 2:
+            alert("Saliendo del programa...");
+            break;
+        default:
+            alert("Ingrese una opcion valida");
+            break;
+    }
+    opcion=menu();
 }
